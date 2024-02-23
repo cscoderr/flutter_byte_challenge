e07a84bac919cea2789d4d305974e020c9cb72f2
Tomiwa Idowu <tomiwaidowuu@gmail.com>
2024-02-23 14:00:37 +0100

    initial commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,81 +16,78 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFFFFFFFF),
-                        Color(0xFFDCF0FB),
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned.fill(
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFFFFFFF),
+                      Color(0xFFDCF0FB),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                   ),
                 ),
               ),
-              Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    MouseRegion(
-                      cursor: SystemMouseCursors.allScroll,
-                      onHover: (event) {
-                        setState(() {
-                          x = event.localPosition.dx;
-                          y = event.localPosition.dy;
-                        });
+            ),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  MouseRegion(
+                    cursor: SystemMouseCursors.allScroll,
+                    onHover: (event) {
+                      setState(() {
+                        x = event.localPosition.dx;
+                        y = event.localPosition.dy;
+                      });
+                    },
+                    child: ShaderMask(
+                      shaderCallback: (bounds) {
+                        return const RadialGradient(
+                          colors: [
+                            AppConstants.darkBlue,
+                            AppConstants.lightBlue,
+                          ],
+                          radius: 0.6,
+                        ).createShader(
+                          Rect.fromCircle(
+                            center: Offset(x, y),
+                            radius: bounds.width * 0.1,
+                          ),
+                        );
                       },
-                      child: ShaderMask(
-                        shaderCallback: (bounds) {
-                          return const RadialGradient(
-                            colors: [
-                              AppConstants.darkBlue,
-                              AppConstants.lightBlue,
-                            ],
-                            radius: 0.6,
-                          ).createShader(
-                            Rect.fromCircle(
-                              center: Offset(x, y),
-                              radius: bounds.width * 0.1,
-                            ),
-                          );
-                        },
-                        blendMode: BlendMode.srcIn,
-                        child: Text(
-                          'FlutterBytes',
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayLarge
-                              ?.copyWith(
-                                color: AppConstants.lightBlue,
-                              ),
-                        ),
+                      blendMode: BlendMode.srcIn,
+                      child: Text(
+                        'FlutterBytes',
+                        style:
+                            Theme.of(context).textTheme.displayLarge?.copyWith(
+                                  color: AppConstants.lightBlue,
+                                ),
                       ),
                     ),
-                  ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 90,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Center(
+                child: Text(
+                  'Community',
+                  style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                        fontWeight: FontWeight.w400,
+                      ),
                 ),
               ),
-              Positioned(
-                top: 90,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Center(
-                  child: Text(
-                    'Community',
-                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                          fontWeight: FontWeight.w400,
-                        ),
-                  ),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
